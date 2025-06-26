846b99446d7bd26cc96aa0c93459b04fe7b1ae57
Andreas Almen <163128954+Pumpkinpoem@users.noreply.github.com>
2025-06-26 12:07:30 +0200

    refactor(icons): extract ChevronDownIcon to shared ChevronIcons.tsx and update Sidebar to use it
    - Moved ChevronDownIcon from Sidebar.tsx to components/common/icons/ChevronIcons.tsx
    - Updated Sidebar.tsx to import ChevronDownIcon
    - Improves icon reusability and code maintainability

--- components/common/icons/ChevronIcons.tsx
+++ components/common/icons/ChevronIcons.tsx
@@ -39,13 +39,8 @@ export const ChevronRightIcon: React.FC<IconProps> = ({ className = "", size = '
   </svg>
 );
 
-export const ChevronDownIcon: React.FC<IconProps> = ({ className = "", size = 'md' }) => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    viewBox="0 0 20 20" 
-    fill="currentColor" 
-    className={`${getSizeClasses(size)} ${className}`}
-  >
+export const ChevronDownIcon: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className}>
     <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.23 8.29a.75.75 0 01.02-1.06z" clipRule="evenodd" />
   </svg>
 );
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -4,6 +4,7 @@ import { SphereDisplay } from '../../src/features/spheres/components/SphereDispl
 import { useSphere } from '../../context/SphereContext';
 import { useUser } from '../../context/UserContext';
 import { useModal } from '../../context';
+import { ChevronDownIcon } from '../common/icons/ChevronIcons';
 
 interface SidebarProps {
   currentPath: string;
@@ -52,12 +53,6 @@ const ChevronLeftIcon = ({ className = "w-5 h-5" }: { className?: string }) => (
 const ChevronRightIcon = ({ className = "w-5 h-5" }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-  </svg>
-);
-
-const ChevronDownIcon: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className}>
-    <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.23 8.29a.75.75 0 01.02-1.06z" clipRule="evenodd" />
   </svg>
 );
 
