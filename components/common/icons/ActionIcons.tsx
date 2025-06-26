ad3b24d46ca8643dd20858aa14c76c72d10d4b71
Andreas Almen <163128954+Pumpkinpoem@users.noreply.github.com>
2025-06-26 12:16:16 +0200

    refactor(icons): extract PhotoIcon to shared ActionIcons.tsx and update Sidebar to use it
    - Moved PhotoIcon from Sidebar.tsx to components/common/icons/ActionIcons.tsx
    - Updated Sidebar.tsx to import PhotoIcon
    - Improves icon reusability and code maintainability

--- components/common/icons/ActionIcons.tsx
+++ components/common/icons/ActionIcons.tsx
@@ -23,4 +23,10 @@ export const PaintBrushIcon: React.FC<{ className?: string }> = ({ className = "
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
   </svg>
+);
+
+export const PhotoIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+  </svg>
 ); 
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -6,6 +6,7 @@ import { useUser } from '../../context/UserContext';
 import { useModal } from '../../context';
 import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from '../common/icons/ChevronIcons';
 import { PlusCircleIcon, UserPlusIcon, Cog6ToothIcon, PaintBrushIcon } from '../common/icons/ActionIcons';
+import { PhotoIcon } from '../common/icons/ActionIcons';
 
 interface SidebarProps {
   currentPath: string;
@@ -44,12 +45,6 @@ const NAV_ITEMS_SIDEBAR = [
       </svg> 
   },
 ];
-
-const PhotoIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-    </svg>
-);
 
 const UsersIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
