1169f555d7df891e73038766b860c4706f590db3
Andreas Almen <163128954+Pumpkinpoem@users.noreply.github.com>
2025-06-26 12:14:13 +0200

    refactor(icons): extract Cog6ToothIcon to shared ActionIcons.tsx and update Sidebar to use it
    - Moved Cog6ToothIcon from Sidebar.tsx to components/common/icons/ActionIcons.tsx
    - Updated Sidebar.tsx to import Cog6ToothIcon
    - Improves icon reusability and code maintainability

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -5,7 +5,7 @@ import { useSphere } from '../../context/SphereContext';
 import { useUser } from '../../context/UserContext';
 import { useModal } from '../../context';
 import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from '../common/icons/ChevronIcons';
-import { PlusCircleIcon, UserPlusIcon } from '../common/icons/ActionIcons';
+import { PlusCircleIcon, UserPlusIcon, Cog6ToothIcon } from '../common/icons/ActionIcons';
 
 interface SidebarProps {
   currentPath: string;
@@ -44,13 +44,6 @@ const NAV_ITEMS_SIDEBAR = [
       </svg> 
   },
 ];
-
-const Cog6ToothIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => ( 
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.646.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.43.992a6.759 6.759 0 010 1.255c-.008.378.138.75.43.99l1.004.827a1.125 1.125 0 01.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 01-.22.128c-.333.183-.582.495-.644.869l-.213 1.28c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.759 6.759 0 010-1.255c.008-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-  </svg>
-);
 
 const PaintBrushIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
